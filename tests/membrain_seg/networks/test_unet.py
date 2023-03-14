0e47139f8be0d5541e1efab48e210e3ef1cc6f15
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2023-03-14 20:45:42 +0100

    move import under warning filter

--- tests/membrain_seg/networks/test_unet.py
+++ tests/membrain_seg/networks/test_unet.py
@@ -1,6 +1,9 @@
-from membrain_seg.networks import SemanticSegmentationUnet
+import pytest
 
 
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_semantic_segmentation_unet():
     """Simple smoke test for instantiating SemanticSegmentationUnet."""
+    from membrain_seg.networks import SemanticSegmentationUnet
+
     _ = SemanticSegmentationUnet()
