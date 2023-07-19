6e96830d1bf071f2156e361152431f72042517ec
LorenzLamm <lorenz.lamm@tum.de>
2023-07-19 12:17:06 +0200

    fix test time augmentation number

--- src/membrain_seg/segmentation/segment.py
+++ src/membrain_seg/segmentation/segment.py
@@ -89,7 +89,7 @@ def segment(
     # Perform test time augmentation (8-fold mirroring)
     predictions = torch.zeros_like(new_data)
     print("Performing 8-fold test-time augmentation.")
-    for m in range(1):
+    for m in range(8):
         with torch.no_grad():
             predictions += (
                 get_mirrored_img(
