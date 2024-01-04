a38fe510d6fefbde7ae38b1333f1f17cb376af2f
LorenzLamm <lorenz.lamm@tum.de>
2024-01-04 13:35:57 +0100

    CSV writing and storing of vectors in VTK

--- src/membrain_seg/segmentation/dataloading/data_utils.py
+++ src/membrain_seg/segmentation/dataloading/data_utils.py
@@ -6,6 +6,7 @@ from typing import Any, Callable, Optional, Union
 import mrcfile
 import numpy as np
 import SimpleITK as sitk
+import vtk
 from skimage.util import img_as_float32
 from torch import Tensor, device
 
@@ -85,6 +86,84 @@ def get_csv_data(csv_path, delimiter=",", with_header=False, return_header=False
     except Exception:
         out_array = np.array(out_array)
     return out_array
+
+
+def store_array_in_csv(out_file: str, data: np.ndarray, out_del: str = ",") -> None:
+    """
+    Store tabular data in a CSV file.
+
+    Parameters
+    ----------
+    out_file : str
+        The output file path where the CSV will be stored.
+    data : np.ndarray
+        The data array to store in the CSV file.
+    out_del : str, optional
+        The delimiter to use in the CSV file, by default ",".
+    """
+    with open(out_file, "w") as out_csv:
+        csv_writer = csv.writer(out_csv, delimiter=out_del)
+        for i in range(data.shape[0]):
+            row = data[i]
+            csv_writer.writerow(row)
+
+
+def store_point_and_vectors_in_vtp(
+    out_path: str,
+    in_points: np.ndarray,
+    in_vectors: np.ndarray = None,
+):
+    """
+    Store points and, optionally, their associated vectors into a VTP file.
+
+    This function takes an array of points and an optional array of vectors
+    corresponding to each point and stores them in a VTK PolyData format,
+    which is then written to a VTP file.
+
+    Parameters
+    ----------
+    out_path : str
+        The path to the desired output VTP file.
+    in_points : np.ndarray
+        A Numpy array of points where each point is represented as [x, y, z].
+        Shape should be (n_points, 3).
+    in_vectors : np.ndarray, optional
+        A Numpy array of vectors associated with each point, typically representing
+        normals or other vector data. Shape should be (n_points, 3). If not provided,
+        only point data is written to the VTP file.
+
+    Returns
+    -------
+    None
+        This function does not return a value. It writes directly to the specified file.
+
+    Raises
+    ------
+    IOError
+        If there is an error writing the file, an error message is printed.
+    """
+    points = vtk.vtkPoints()
+    for point in in_points:
+        points.InsertNextPoint(point[0], point[1], point[2])
+
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(points)
+
+    if in_vectors is not None:
+        vectors = vtk.vtkDoubleArray()
+        vectors.SetNumberOfComponents(3)
+        vectors.SetName("Normal")
+        for vector in in_vectors:
+            vectors.InsertNextTuple(vector)
+        polydata.GetPointData().AddArray(vectors)
+        polydata.GetPointData().SetActiveVectors(vectors.GetName())
+
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetFileName(out_path)
+    writer.SetInputData(polydata)
+    if writer.Write() != 1:
+        error_msg = "Error writing the file"
+        print(error_msg)
 
 
 def load_data_for_inference(data_path: str, transforms: Callable, device: device):
