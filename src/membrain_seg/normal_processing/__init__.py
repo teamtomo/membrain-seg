acd77623acaff8d1de1a9a725a6f60be5cc2d6f7
LorenzLamm <lorenz.lamm@tum.de>
2024-01-07 15:05:06 +0100

    update init

--- src/membrain_seg/normal_processing/__init__.py
+++ src/membrain_seg/normal_processing/__init__.py
@@ -1,3 +1,6 @@
 """Normal processing module for membrain-seg."""
 from .cli import cli  # noqa: F401
-from .extract_normals import extract_normals  # noqa: F401
+from .extract_normals import (  # noqa: F401
+    extract_normals_GT,
+    match_coords_to_membrane_normals,
+)
