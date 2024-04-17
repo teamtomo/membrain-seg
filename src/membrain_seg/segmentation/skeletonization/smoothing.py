0d8b9de679f1d773ff8dbf42079dc71d8042cf6f
Hanyi Zhang <hanyi.zhang@hpc-build01.scidom.de>
2024-04-17 13:57:07 +0200

    Third commit

--- src/membrain_seg/segmentation/skeletonization/smoothing.py
+++ src/membrain_seg/segmentation/skeletonization/smoothing.py
@@ -1,17 +1,19 @@
 import torch
-import torch.nn.functional as F
+
 from membrain_seg.segmentation.training.surface_dice import (
     apply_gaussian_filter,
-    gaussian_kernel
 )
 
-def process_hessian_tensors(hessian_components: list, kernel_size: int = 9, sigma: float = 1.0) -> list:
+
+def process_hessian_tensors(
+    hessian_components: list, kernel_size: int = 9, sigma: float = 1.0
+) -> list:
     """
     Processes a list of Hessian component matrices by applying a Gaussian filter to each.
 
     This function takes a list of numpy arrays representing different components of the Hessian matrix
     (e.g., HessianXX, HessianYY, etc.), converts them to PyTorch tensors, applies a Gaussian filter to each,
-    and returns the filtered matrices as numpy arrays. The function automatically handles device selection 
+    and returns the filtered matrices as numpy arrays. The function automatically handles device selection
     (CPU or GPU) based on availability and uses a dictionary to store and reuse Gaussian kernels for efficiency.
 
     Parameters
@@ -35,12 +37,17 @@ def process_hessian_tensors(hessian_components: list, kernel_size: int = 9, sigm
     >>> len(filtered_components)
     6
     """
-    gaussian_kernel_dict = {}
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
-    tensor_components = [torch.from_numpy(comp).float().to(device).unsqueeze(0).unsqueeze(0) for comp in hessian_components]
-    
-    filtered_tensors = [apply_gaussian_filter(tensor, kernel_size, sigma) for tensor in tensor_components]
+    tensor_components = [
+        torch.from_numpy(comp).float().to(device).unsqueeze(0).unsqueeze(0)
+        for comp in hessian_components
+    ]
+
+    filtered_tensors = [
+        apply_gaussian_filter(tensor, kernel_size, sigma)
+        for tensor in tensor_components
+    ]
     filtered_arrays = [tensor.cpu().numpy().squeeze() for tensor in filtered_tensors]
-    
+
     return filtered_arrays
