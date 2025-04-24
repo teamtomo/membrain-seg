077966748327f6e195e643a41992d54a87bd63be
LorenzLamm <lorenz.lamm@googlemail.com>
2025-04-24 13:43:45 +0200

    enable cpu device

--- src/membrain_seg/segmentation/cli/ske_cli.py
+++ src/membrain_seg/segmentation/cli/ske_cli.py
@@ -22,6 +22,11 @@ def skeletonize(
         "the entire volume is processed at once. If operating with limited GPU "
         "resources, a batch size of 1,000,000 is recommended.",
     ),
+    device: str = Option(  # noqa: B008
+        None,
+        help="Device to use for processing. If not specified, the default device "
+        "will be used. Options are 'cpu' or 'cuda'.",
+    ),
 ):
     """
     Perform skeletonization on labeled tomograms using nonmax-suppression technique.
@@ -72,7 +77,7 @@ def skeletonize(
     print("")
 
     segmentation = load_tomogram(label_path)
-    ske = _skeletonization(segmentation=segmentation.data, batch_size=batch_size)
+    ske = _skeletonization(segmentation=segmentation.data, batch_size=batch_size, device=device)
 
     # Update the segmentation data with the skeletonized output while preserving the
     # original header and voxel_size
